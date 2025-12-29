b37e029f2e4b83f13036fb20c9067bb2e516a1e8
Ziyodullo Gaybullaev <124051204+DataScienceShow@users.noreply.github.com>
2025-12-29 13:30:12 +0500

    Fetch smoothie data and display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
-    # st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+    
     my_insert_stmt = """ INSERT INTO smoothies.public.orders (INGREDIENTS, NAME_ON_ORDER)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     # st.write(my_insert_stmt)
@@ -42,9 +44,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
-# st.text(smoothiefroot_response.json())
+
 
 
         
